cb30d1c428d252bf66c5a077825909a50fb0d997
KlaudiaZabornia <100691773+kzaborniak@users.noreply.github.com>
2022-03-15 12:26:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int i = 0;
+      System.out.println("Pętla for");
+    for (i =2; i<=100; i++){
+      if( i%2==0){
+        System.out.println(i);
+      }
+    }
+  }
+}
